dff7c708a4f1299c53a454166bf623f6dcc938db
FathanOrangjawa <164990358+FathanOrangjawa@users.noreply.github.com>
2025-03-06 15:12:47 +0700

    Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,12 @@
-function order(paket, harga) {
-    var nomor = "6285293393972"; // Ganti dengan nomor WA lu
-    var pesan = `Halo, saya ingin memesan paket ${paket}.\nDetail: ${harga}`;
-    var link = `https://wa.me/${nomor}?text=${encodeURIComponent(pesan)}`;
-    window.open(link, "_blank");
+function openModal(id) {
+    document.getElementById("modal-" + id).style.display = "block";
+}
+
+function closeModal(id) {
+    document.getElementById("modal-" + id).style.display = "none";
+}
+
+function toggleMenu() {
+    var menu = document.querySelector(".navbar-menu");
+    menu.classList.toggle("active");
 }
